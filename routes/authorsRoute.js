9cf671bd7023054471c43003e1fe571fed51cde7
Jayson-Roda <roda.jayson.a@gmail.com>
2022-08-26 10:08:52 +0800

    Initial Authors Routh

---
+++ routes/authorsRoute.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/authorSchema')
+
+// All authors Route
+router.get('/', async(req, res) => {
+    let searchOptions = {}
+    if (req.query.name !== null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render("authors/index", { 
+            authors,
+            searchOptions: req.query 
+            })
+    } catch {
+        res.redirect('/')
+    }
+    
+})
+
+// New Author Route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
+})
+
+// Crete Author Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch {
+        res.render('authors/new', {
+            author,
+            errorMessage: 'Error Creating Author'
+        })
+    }
+    // author.save((err, newAuthor) => {
+    //     if(err){
+    //         res.render('authors/new', {
+    //             author: author,
+    //             errorMessage: 'Error Creating Author'
+    //         })
+    //     } else {
+            // // res.redirect(`authors/${newAuthor.id}`)
+            // res.redirect(`authors`)
+    //     }
+    // })
+    // res.send(req.body.name)
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/indexRoutes')
+const authorRouter = require('./routes/authorsRoute')
 
 // MIDDLEWARE
 app.set('view engine', 'ejs')
@@ -15,6 +17,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 
 mongoose.connect(process.env.DATABASE_URL, {
@@ -26,5 +29,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log("Connected to mongoose"))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
